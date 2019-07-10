2db3ac0a022c90594392f5778510fddb5e20b39f
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-10 15:38:59 -0500

    Added skeleton for API call to weather service to retrieve realtime information

--- lemonadeStand/Program.cs
+++ lemonadeStand/Program.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace lemonadeStand
 {
     class Program
     {
+        private const string URL = "api.openweathermap.org/data/2.5/weather";
+        public string urlParameters = "?q=Milwaukee";
         static void Main(string[] args)
         {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync("?q=Milwaukee").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             Game game = new Game();
             game.RunGame();
+            }
         }
     }
 }
